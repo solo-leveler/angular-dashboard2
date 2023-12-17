4a05a5540c39a68f5b4889cc4488132bf8240d06
solo-leveler <shinichiizumi@protonmail.ch>
2023-12-18 00:12:55 +0530

    added header-component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(
+    library: FaIconLibrary
+  ) {
+   library.addIconPacks(fas, far);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
