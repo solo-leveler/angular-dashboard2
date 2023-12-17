d8ac11c52e1a2d688fcb36262527478c1842eefd
solo-leveler <shinichiizumi@protonmail.ch>
2023-12-18 00:06:34 +0530

    added app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { DashboardRightComponent } from './dashboard-right/dashboard-right.component';
+import { DashboardLeftComponent } from './dashboard-left/dashboard-left.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LineChartComponent } from './dashboard-right/line-chart/line-chart.component';
+import { BalanceComponent } from './dashboard-right/balance/balance.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { TransactionHistoryComponent } from './dashboard-right/transaction-history/transaction-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HeaderComponent,
+    DashboardRightComponent,
+    DashboardLeftComponent,
+    LineChartComponent,
+    BalanceComponent,
+    TransactionHistoryComponent
   ],
   imports: [
-    BrowserModule
+    NgApexchartsModule,
+    BrowserModule,
+    FontAwesomeModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
